1d26b5a4588d4a2bc3ac0703d63888f9c3e86490
Dan-H00 <hotico.dan00@yahoo.com>
2023-06-18 23:59:24 +0300

    first commit

---
+++ src/main/java/dan/hotico/dbmatch/DbMatchApplication.java
@@ -0,0 +1,13 @@
+package dan.hotico.dbmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/hotico/dbmatch/DbMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.hotico.dbmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
